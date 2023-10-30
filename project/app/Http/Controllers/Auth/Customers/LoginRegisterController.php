56a8e0f31bd5bfc87c85e7338b7ed62cda4984df
amin <AMTeam.web666@gmail.com>
2023-10-30 13:14:18 +0330

    create Login Contoller and add form show method and login method to controller

--- project/app/Http/Controllers/Auth/Customers/LoginRegisterController.php
+++ project/app/Http/Controllers/Auth/Customers/LoginRegisterController.php
@@ -2,12 +2,68 @@
 
 namespace App\Http\Controllers\Auth\Customers;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\Customers\LoginRegisterRequest;
+use App\Models\Otp;
 
 class LoginRegisterController extends Controller
 {
-    public function loginRegisterForm(){
-        return view("customers.auth.login-register");
+    public function loginRegister(LoginRegisterRequest $request)
+    {
+        $inputs = $request->all();
+
+        //check id is email or not
+        if(filter_var($inputs['id'], FILTER_VALIDATE_EMAIL))
+        {
+            $type = 1; // 1 => email
+            $user = User::where('email', $inputs['id'])->first();
+            if(empty($user)){
+                $newUser['email'] = $inputs['id'];
+            }
+        }
+
+        //check id is mobile or not
+        elseif(preg_match('/^(\+98|98|0)9\d{9}$', $inputs['id'])){
+            $type = 0; // 0 => mobile;
+
+
+            // all mobile numbers are in on format 9** *** ***
+            $inputs['id'] = ltrim($inputs['id'], '0');
+            $inputs['id'] = substr($inputs['id'], 0, 2) === '98' ? substr($inputs['id'], 2) : $inputs['id'];
+            $inputs['id'] = str_replace('+98', '', $inputs['id']);
+
+            $user = User::where('mobile', $inputs['id'])->first();
+            if(empty($user)){
+                $newUser['mobile'] = $inputs['id'];
+            }
+        }
+
+        else{
+            $errorText = 'شناسه ورودی شما نه شماره موبایل است نه ایمیل';
+            return redirect()->route('auth.customer.login-register-form')->withErrors(['id' => $errorText]);
+        }
+
+        if(empty($user)){
+            $newUser['password'] = '98525114';
+            $newUser['activation'] = 1;
+            $user = User::create($newUser);
+        }
+
+        //create otp code
+        $otpCode = rand(111111, 999999);
+        $token = Str::random(60);
+        $otpInputs = [
+            'otp_code' => $otpCode,
+            'token' => $token,
+            'user_id' => $user->id,
+            'login_id' => $inputs['id'],
+            'type' => $type,
+        ];
+
+        Otp::create($otpInputs);
+
     }
 }
